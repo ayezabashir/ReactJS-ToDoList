2446a7d9f5652bbbf6fb7784f956b9d84f01c200
Ayeza <ayezabashir442@gmail.com>
2023-06-22 22:39:38 +0500

    adding styles

---
+++ src/components/ToDo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToDo = () => {
+    return (
+        <div>
+            To Do
+        </div>
+    )
+}
+
+export default ToDo
---
+++ src/components/ToDoWrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToDoWrapper = () => {
+    return (
+        <div>
+            To do ToDoWrapper
+        </div>
+    )
+}
+
+export default ToDoWrapper
